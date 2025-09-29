f5bf0866ddaa37b4600ca08338b8ae6955762d88
Bo_Sislach <146647133+BoSislach@users.noreply.github.com>
2025-09-29 18:24:25 +0200

    Rename parameters in stampaGriglia method
    Updated parameter names in stampaGriglia method for consistency.

--- Progetto.java
+++ Progetto.java
@@ -37,8 +37,19 @@ public class Progetto {
     }
 
 
-    public static void stampaGriglia(char[][] griglia, int righe, int colonne) {
-        
+    public static void stampaGriglia(char[][] griglia, int RIGHE, int COLONNE) {
+        System.out.println();
+        for (int i = 0; i < RIGHE; i++) {
+            System.out.print("|");
+            for (int j = 0; j < COLONNE; j++) {
+                System.out.print(griglia[i][j]);
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+        System.out.println("---------------");
+        System.out.println(" 1 2 3 4 5 6 7 ");
+        System.out.println();
     }
 
 
