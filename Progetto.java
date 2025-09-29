cd3cd730d4bf79ece007662b0bd39e47a78b1325
Bo_Sislach <146647133+BoSislach@users.noreply.github.com>
2025-09-29 18:19:03 +0200

    Update Progetto.java

--- Progetto.java
+++ Progetto.java
@@ -24,7 +24,7 @@ public class Progetto {
     }
 
     static void inizializzaGriglia(char[][] griglia, int RIGHE, int COLONNE, char VUOTO) {
-        for (int i = 0; i < RIGHE; j++) {
+        for (int i = 0; i < RIGHE; i++) {
             for (int j = 0; j < COLONNE; j++) {
                 griglia[i][j] = VUOTO;
             }
