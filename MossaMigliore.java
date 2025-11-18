3eb053d8dd45854fafbfce284dba1e47116c3c9a
Bo_Sislach <146647133+BoSislach@users.noreply.github.com>
2025-11-18 19:53:34 +0100

    Update MossaMigliore.java

--- MossaMigliore.java
+++ MossaMigliore.java
@@ -1,22 +1,25 @@
-public class MossaMigliore{
-    public static int mossaMigliore(char[][] griglia, int RIGHE, int COLONNE, char simboloGiocatore, char simboloAvversario){
-        for(int colonna = 0; colonna < COLONNE; colonna++){
-            if(PosizioneValida.posizioneValida(griglia, colonna, ' ')){
+public class MossaMigliore {
+
+    public static int mossaMigliore(char[][] griglia, int RIGHE, int COLONNE, char simboloComputer, char simboloGiocatore) {
+
+        for (int colonna = 0; colonna < COLONNE; colonna++) {
+            if (PosizioneValida.posizioneValida(griglia, colonna, ' ')) {
                 int riga = ProssimaRigaVuota.prossimaRigaVuota(griglia, colonna);
-                griglia[riga][colonna] = simboloAvversario;
-                if(ControllaVincita.controllaVincita(griglia, RIGHE, COLONNE, simboloAvversario)){
-                    griglia[riga][colonna] = ' ';
+                griglia[riga][colonna] = simboloGiocatore; 
+                if (ControllaVincita.controllaVincita(griglia, RIGHE, COLONNE, simboloGiocatore)) {
+                    griglia[riga][colonna] = ' '; 
                     return colonna;
                 }
                 griglia[riga][colonna] = ' ';
             }
         }
 
-        for(int colonna = 0; colonna < COLONNE; colonna++){
-            if(PosizioneValida.posizioneValida(griglia, colonna, ' ')){
+        for (int colonna = 0; colonna < COLONNE; colonna++) {
+            if (PosizioneValida.posizioneValida(griglia, colonna, ' ')) {
                 return colonna;
             }
         }
-        return -1;
+
+        return -1; 
     }
 }
