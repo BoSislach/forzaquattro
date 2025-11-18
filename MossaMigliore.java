01cfdaee0900f67850dba053e60fb31076c3b448
Bo_Sislach <146647133+BoSislach@users.noreply.github.com>
2025-11-18 21:43:48 +0100

    Update MossaMigliore.java

--- MossaMigliore.java
+++ MossaMigliore.java
@@ -1,32 +1,36 @@
-public class MossaComputer {
-    public static char inserisciSimbolo(char[][] griglia, char turno, int COLONNE, char simboloGiocatore1, char simboloGiocatore2) {
-        int turniComputer = 0;
+public class MossaMigliore {
+    public static int mossaMigliore(char[][] griglia, int RIGHE, int COLONNE, char simboloComputer, char simboloGiocatore) {
         
-        if (turno == simboloGiocatore1) {
-            return InserisciSimbolo.inserisciSimbolo(griglia, turno, COLONNE, simboloGiocatore1, simboloGiocatore2);
-        }
-        
-        turniComputer++;
-        int colonna;
-        
-        if (turniComputer <= 3) {
-            do {
-                colonna = (int) (Math.random() * COLONNE);
-            } while (!PosizioneValida.posizioneValida(griglia, colonna, ' '));
-        } else {
-            colonna = MossaMigliore.mossaMigliore(griglia, griglia.length, COLONNE, simboloGiocatore2, simboloGiocatore1);
-            
-            if (colonna == -1) {
-                do {
-                    colonna = (int) (Math.random() * COLONNE);
-                } while (!PosizioneValida.posizioneValida(griglia, colonna, ' '));
+        for (int colonna = 0; colonna < COLONNE; colonna++) {
+            if (PosizioneValida.posizioneValida(griglia, colonna, ' ')) {
+                int riga = ProssimaRigaVuota.prossimaRigaVuota(griglia, colonna);
+                griglia[riga][colonna] = simboloComputer;
+                if (ControllaVincita.controllaVincita(griglia, RIGHE, COLONNE, simboloComputer)) {
+                    griglia[riga][colonna] = ' '; 
+                    return colonna; 
+                }
+                griglia[riga][colonna] = ' '; 
             }
         }
-        
-        int riga = ProssimaRigaVuota.prossimaRigaVuota(griglia, colonna);
-        griglia[riga][colonna] = simboloGiocatore2;
-        System.out.println("Computer sceglie la colonna " + (colonna + 1));
-        
-        return simboloGiocatore1;
+
+        for (int colonna = 0; colonna < COLONNE; colonna++) {
+            if (PosizioneValida.posizioneValida(griglia, colonna, ' ')) {
+                int riga = ProssimaRigaVuota.prossimaRigaVuota(griglia, colonna);
+                griglia[riga][colonna] = simboloGiocatore; 
+                if (ControllaVincita.controllaVincita(griglia, RIGHE, COLONNE, simboloGiocatore)) {
+                    griglia[riga][colonna] = ' '; 
+                    return colonna;
+                }
+                griglia[riga][colonna] = ' ';
+            }
+        }
+
+        for (int colonna = 0; colonna < COLONNE; colonna++) {
+            if (PosizioneValida.posizioneValida(griglia, colonna, ' ')) {
+                return colonna;
+            }
+        }
+
+        return -1; 
     }
 }
