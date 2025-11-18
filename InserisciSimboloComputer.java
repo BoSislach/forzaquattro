97c83cf1eae2d81e0253717b57fb8b3fb3a5754c
Bo_Sislach <146647133+BoSislach@users.noreply.github.com>
2025-11-18 20:21:11 +0100

    Update InserisciSimboloComputer.java

--- InserisciSimboloComputer.java
+++ InserisciSimboloComputer.java
@@ -1,23 +1,38 @@
+import java.util.Scanner;
 public class InserisciSimboloComputer {
-    public static int inserisciSimboloComputer(char[][] griglia, char turno, int COLONNE, char simboloGiocatore1, char simboloComputer) {
-        
-        char pedina = turno;
+
+    public static char inserisciSimboloComputer(char[][] griglia, char turno, int COLONNE, char simboloGiocatore, char simboloComputer) {
+        Scanner scanner = new Scanner(System.in);
+        int turniComputer = 0;
         int colonna;
-        int counterMosseComputer = 0;
-        
-        do {
-            colonna = (int) (Math.random() * COLONNE);
-            counterMosseComputer++;
-            
-        } while (!PosizioneValida.posizioneValida(griglia, colonna, ' ') && counterMosseComputer < 4);
+        int riga;
 
-        do {
-           
-            colonna = MossaMigliore.mossaMigliore(griglia, griglia.length, COLONNE, simboloGiocatore1, simboloComputer);
-            
-        } while (!PosizioneValida.posizioneValida(griglia, colonna, ' '));
+        if (turno == simboloGiocatore) {
+            turno = InserisciSimbolo.inserisciSimbolo(griglia, turno, COLONNE, simboloGiocatore, simboloComputer);
+        } else {
+            turniComputer++;
 
-        System.out.println("Computer sceglie la colonna " + (colonna + 1));
-        return colonna;
+            if (turniComputer <= 3) {
+                do {
+                    colonna = (int) (Math.random() * COLONNE);
+                } while (!PosizioneValida.posizioneValida(griglia, colonna, ' '));
+            } else {
+                colonna = MossaMigliore.mossaMigliore(griglia, griglia.length, COLONNE, simboloComputer, simboloGiocatore);
+                
+                if (colonna == -1) {
+                    do {
+                        colonna = (int) (Math.random() * COLONNE);
+                    } while (!PosizioneValida.posizioneValida(griglia, colonna, ' '));
+                }
+            }
+
+            riga = ProssimaRigaVuota.prossimaRigaVuota(griglia, colonna);
+            griglia[riga][colonna] = simboloComputer;
+            System.out.println("Computer sceglie la colonna " + (colonna + 1));
+
+            turno = simboloGiocatore;
+        }
+
+        return turno;
     }
 }
