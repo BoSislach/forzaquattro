714ca22323e1ba972d19f21e622d7b1cd9ffb2d2
Bo_Sislach <146647133+BoSislach@users.noreply.github.com>
2025-11-18 18:36:26 +0100

    Add files via upload

--- InserisciSimboloComputer.java
+++ InserisciSimboloComputer.java
@@ -1,3 +1,23 @@
 public class InserisciSimboloComputer {
-    
+    public static int inserisciSimboloComputer(char[][] griglia, char turno, int COLONNE, char simboloGiocatore1, char simboloComputer) {
+        
+        char pedina = turno;
+        int colonna;
+        int counterMosseComputer = 0;
+        
+        do {
+            colonna = (int) (Math.random() * COLONNE);
+            counterMosseComputer++;
+            
+        } while (!PosizioneValida.posizioneValida(griglia, colonna, ' ') && counterMosseComputer < 4);
+
+        do {
+           
+            colonna = MossaMigliore.mossaMigliore(griglia, griglia.length, COLONNE, simboloGiocatore1, simboloComputer);
+            
+        } while (!PosizioneValida.posizioneValida(griglia, colonna, ' '));
+
+        System.out.println("Computer sceglie la colonna " + (colonna + 1));
+        return colonna;
+    }
 }
