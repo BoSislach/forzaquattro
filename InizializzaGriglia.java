e035754c7f1d010d7bfa24c74517c03556cd47f1
Bo_Sislach <146647133+BoSislach@users.noreply.github.com>
2025-11-18 16:40:19 +0100

    Update InizializzaGriglia.java

--- InizializzaGriglia.java
+++ InizializzaGriglia.java
@@ -1,5 +1,5 @@
 public class InizializzaGriglia {
-    static void inizializzaGriglia(char[][] griglia, int RIGHE, int COLONNE, char VUOTO) {
+    public static void inizializzaGriglia(char[][] griglia, int RIGHE, int COLONNE, char VUOTO) {
         for (int i = 0; i < RIGHE; i++) {
             for (int j = 0; j < COLONNE; j++) {
                 griglia[i][j] = VUOTO;
